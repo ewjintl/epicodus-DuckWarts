a1a4f5ce9bec1894a2cd51c3332f2edf7b61fcf0
Elliot Jordan <ewjintl@icloud.com>
2020-03-11 19:22:12 -0700

    added into branch story, fixed select options, and hide subsequent cards via css.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
- $("#charNameCard").submit(function(event) {
-event.preventDefault();
-var card = $("input#charName").val();
-
-
+ $("form#charNameCard").submit(function(event) {
+   var charName = $("input#charName").val();
+   console.log(charName);
+   $(".charName").text(charName);
+   $("#introCard").show();
+   event.preventDefault();
  }); 
+});
+
+$(document).ready(function() {
+  $("form#intro").submit(function(event) {
+    var decsionOne = 
+
+  }); 
 });
